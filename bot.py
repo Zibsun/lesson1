90190ffd160a6aa2c805815822772a7796a999f5
Askhat Urazbaev <askhat@gmail.com>
2018-02-17 18:12:05 +0300

    Bot can talk to people

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 	level=logging.INFO,
 	filename='bot.log'
 	)
+
+greeting_text = "Что нового?"
+
+dialog = {"привет": "И тебе привет!", "как дела": "Лучше всех", "пока": "Увидимся"}
 
 def main():
     updater = Updater("526728660:AAEctMCPi-coN2NZpXO0UfatdgWkcBcOzak")
@@ -14,14 +19,14 @@ def main():
     updater.idle()
 
 def greet_user(bot, update):
-	text = 'Вызван /start'
-	print(text) 
-	update.message.reply_text(text)
+	update.message.reply_text(greeting_text)
 
 def talk_to_me(bot, update):
-    user_text = update.message.text 
-    print(user_text)
-    update.message.reply_text(user_text)
+    user_text = update.message.text.lower()
+    reply = "Чо?"
+    if user_text in dialog:
+    	reply = dialog[user_text]
 
+    update.message.reply_text(reply)
 
 main()
